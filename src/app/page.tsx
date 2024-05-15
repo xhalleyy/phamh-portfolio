06e8cde6ea7680baee9757401b298641a6ce1f4c
xhalleyy <xhalleyy@gmail.com>
2024-05-15 05:06:58 -0700

    fixed footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       <div className="bg-[#5A233B] flex flex-1 flex-col">
         <div className=" grid grid-cols-3 gap-5 items-center px-10 md:px-16">
           <div className="col-span-3 xl:col-span-1 grid justify-center">
-            <Image
+            <Image className="h-[280px] w-[280px] md:h-[350px] md:w-[350px] lg:h-[400px] lg:w-[400px] xl:w-auto xl:h-auto"
               src="/me.png"
               width={520}
               height={520}
@@ -119,35 +119,66 @@ export default function Home() {
 
       <div className="bg-[rgb(221,216,213)] relative">
         <div className="px-5 py-10">
-          <h1 className="ps-0 md:ps-7 text-5xl font-railway font-medium text-[rgb(2,88,111)] absolute">Projects</h1>
+          <h1 className="ps-0 md:ps-7 text-5xl font-railway font-bold text-[rgb(2,88,111)] absolute">Projects</h1>
           <ProjectTabsComponent />
         </div>
       </div>
 
-      <div id="contact" className="bg-[rgb(2,88,111)] flex flex-col justify-center items-center text-center pt-16 pb-10">
-        <h1 className="text-[#f5ede4] text-5xl font-medium font-railway">Contact</h1>
-        <div className="font-railway text-[#f5ede4] leading-8 text-xl mt-7 ">
-          <p>hpham@codestack.co</p>
-          <p>(209)-244-8086</p>
-          <p>Stockton, CA</p>
-          <div className="flex flex-row my-6 gap-6 justify-center">
-          <a href="https://www.linkedin.com/in/halleytpham" target="_blank">
-            <Tooltip content="LinkedIn" placement="left" style="light" className="px-4 mt-2 font-railway text-lg">
-              <Image src="/linkedin.png" width={40} height={40} alt="linkedIn" />
-            </Tooltip>
-          </a>
-          <a href="https://drive.google.com/file/d/1eMjkuJP6hVoYu0DrmH44ydaaGW9pDsGd/view?usp=sharing" target="_blank">
-            <Tooltip content="Résumé" placement="bottom" style="light" className="px-4 mt-2 font-railway text-lg">
-              <Image src="/contact-res.png" width={40} height={40} alt="linkedIn" />
-            </Tooltip>
-          </a>
-          <a href="https://github.com/xhalleyy" target="_blank">
-            <Tooltip content="GitHub" placement="right" style="light" className="px-4 mt-2 font-railway text-lg">
-              <Image src="/contact-github.png" width={40} height={40} alt="linkedIn" />
-            </Tooltip>
-          </a>
+      <div id="contact" className="bg-[rgb(2,88,111)] py-8">
+        <div className="grid grid-cols-2 justify-center items-center">
+          <div className="col-span-2 lg:col-span-1 text-center lg:text-start lg:ps-16 xl:ps-32">
+            <h1 className="text-[#f5ede4] text-4xl font-bold font-railway">Contact</h1>
+            <div className="font-railway font-medium ps-1.5 text-[#f5ede4] leading-7 text-xl mt-3 ">
+              <p>hpham@codestack.co</p>
+              <p>(209)-244-8086</p>
+              <p>Stockton, CA</p>
+            </div>
           </div>
+          <div className="col-span-2 lg:col-span-1 lg:pe-16 xl:pe-40 ">
+              <div className="flex flex-row mt-6 gap-6 justify-center lg:justify-end">
+                <a href="https://www.linkedin.com/in/halleytpham" target="_blank">
+                  <Tooltip content="LinkedIn" placement="left" style="light" className="px-4 mt-2 font-railway text-lg">
+                    <Image src="/linkedinwhite.png" width={47} height={47} alt="linkedIn" />
+                  </Tooltip>
+                </a>
+                <a href="https://drive.google.com/file/d/1eMjkuJP6hVoYu0DrmH44ydaaGW9pDsGd/view?usp=sharing" target="_blank">
+                  <Tooltip content="Résumé" placement="top" style="light" className="px-4 mt-2 font-railway text-lg">
+                    <Image src="/resumewhite.png" width={48} height={48} alt="linkedIn" />
+                  </Tooltip>
+                </a>
+                <a href="https://github.com/xhalleyy" target="_blank">
+                  <Tooltip content="GitHub" placement="right" style="light" className="px-4 mt-2 font-railway text-lg">
+                    <Image src="/github.png" width={49} height={49} alt="linkedIn" />
+                  </Tooltip>
+                </a>
+              </div>
+            </div>
         </div>
+        {/* <h1 className="text-[#f5ede4] text-4xl font-bold font-railway">Contact</h1>
+        <div className="font-railway font-medium text-[#f5ede4] leading-8 text-xl mt-3 ">
+          <div>
+            <p>hpham@codestack.co</p>
+            <p>(209)-244-8086</p>
+            <p>Stockton, CA</p>
+          </div>
+          <div className="flex flex-row mt-6  gap-6 justify-center">
+            <a href="https://www.linkedin.com/in/halleytpham" target="_blank">
+              <Tooltip content="LinkedIn" placement="left" style="light" className="px-4 mt-2 font-railway text-lg">
+                <Image src="/linkedin.png" width={40} height={40} alt="linkedIn" />
+              </Tooltip>
+            </a>
+            <a href="https://drive.google.com/file/d/1eMjkuJP6hVoYu0DrmH44ydaaGW9pDsGd/view?usp=sharing" target="_blank">
+              <Tooltip content="Résumé" placement="top" style="light" className="px-4 mt-2 font-railway text-lg">
+                <Image src="/contact-res.png" width={40} height={40} alt="linkedIn" />
+              </Tooltip>
+            </a>
+            <a href="https://github.com/xhalleyy" target="_blank">
+              <Tooltip content="GitHub" placement="right" style="light" className="px-4 mt-2 font-railway text-lg">
+                <Image src="/contact-github.png" width={40} height={40} alt="linkedIn" />
+              </Tooltip>
+            </a>
+          </div>
+        </div> */}
       </div>
     </div>
   );
